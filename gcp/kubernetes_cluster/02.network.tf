c3f48bcd2006a85b3c0612a861ca9f1c72f5eb98
Julian <juliantellezmendez@gmail.com>
2020-12-23 23:44:53 +0000

    chore: add internal firewall

--- gcp/kubernetes_cluster/02.network.tf
+++ gcp/kubernetes_cluster/02.network.tf
@@ -19,3 +19,30 @@ resource "google_compute_subnetwork" "vpc-subnetwork" {
   ip_cidr_range = "10.240.0.0/24"
   network       = google_compute_network.vpc-network.id
 }
+
+
+# docs: https://cloud.google.com/sdk/gcloud/reference/compute/firewall-rules/create
+# example: gcloud compute firewall-rules create <name> --allow tcp,udp,icmp --network <n> --source-ranges <r>
+# usage: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
+
+resource "google_compute_firewall" "internal" {
+  name        = "${var.namespace}-firewall-internal"
+  description = "Internal k8s firewall"
+  project     = google_project.k8s_project.project_id
+  network     = google_compute_network.vpc-network.id
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
+}
