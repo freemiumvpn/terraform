f0b268c122940d8f460ef14d8bf263a6996f77fc
Julian <juliantellezmendez@gmail.com>
2020-12-24 00:51:31 +0000

    chore: add controller instances

---
+++ gcp/kubernetes_cluster/04.instances.tf
@@ -0,0 +1,40 @@
+# docs: https://cloud.google.com/sdk/gcloud/reference/compute/addresses/create
+# example: gcloud compute addresses create ...
+# usage: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+resource "google_compute_instance" "controller" {
+  for_each = var.controller_instances
+
+  project      = google_project.k8s_project.project_id
+  zone         = var.gcp_zone
+  name         = "${var.namespace}-controller-${each.key}"
+  machine_type = "e2-medium"
+  description  = "k8s controller node"
+
+  boot_disk {
+    auto_delete = true
+    mode        = "READ_WRITE"
+    initialize_params {
+      image = "ubuntu-2004-focal-v20201211"
+      size  = 100
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc-network.id
+    subnetwork = google_compute_subnetwork.vpc-subnetwork.id
+    network_ip = "10.240.0.1${each.key}"
+  }
+
+  service_account {
+    scopes = [
+      "compute-rw",
+      "storage-ro",
+      "service-management",
+      "service-control",
+      "logging-write",
+      "monitoring",
+    ]
+  }
+
+  can_ip_forward = true
+}
