e938b1cde8558a2737b078ff35850d965cbe1859
Julian <juliantellezmendez@gmail.com>
2020-12-24 01:02:36 +0000

    chore: add worker instances

--- gcp/kubernetes_cluster/04.instances.tf
+++ gcp/kubernetes_cluster/04.instances.tf
@@ -38,3 +38,46 @@ resource "google_compute_instance" "controller" {
 
   can_ip_forward = true
 }
+
+
+resource "google_compute_instance" "worker" {
+  for_each = var.worker_instances
+
+  project      = google_project.k8s_project.project_id
+  zone         = var.gcp_zone
+  name         = "${var.namespace}-worker-${each.key}"
+  machine_type = "e2-medium"
+  description  = "k8s worker node"
+
+  boot_disk {
+    auto_delete = true
+    mode        = "READ_WRITE"
+    initialize_params {
+      image = "ubuntu-2004-focal-v20201211"
+      size  = 100
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc-network.id
+    subnetwork = google_compute_subnetwork.vpc-subnetwork.id
+    network_ip = "10.240.0.2${each.key}"
+  }
+
+  metadata = {
+    pod-cidr = "10.200.${each.key}.0/24"
+  }
+
+  service_account {
+    scopes = [
+      "compute-rw",
+      "storage-ro",
+      "service-management",
+      "service-control",
+      "logging-write",
+      "monitoring",
+    ]
+  }
+
+  can_ip_forward = true
+}
