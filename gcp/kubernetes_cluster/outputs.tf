512124a44424725c6b77c801e24a9f6dcf214673
Julian <juliantellezmendez@gmail.com>
2021-02-09 20:29:24 +0000

    refactor: delete old infra

--- gcp/kubernetes_cluster/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "project_id" {
-  value = google_project.k8s_project.project_id
-}
-
-output "static_ip" {
-  value = google_compute_address.k8s-address.address
-}
--- gcp/kubernetes_cluster/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  version = "~> 3.42.0"
-  region  = var.gcp_region
-}
-
-data "google_client_config" "default" {}
